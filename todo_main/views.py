c8eddfb73a677ccd6730663ffa8e318d1b48bcbc
achyut <achyutp10@gmail.com>
2023-11-02 19:15:05 +0545

    other updates

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from todo.models import Task
+
 
 def home(request):
-  return render(request, 'home.html')
+  tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+  context = {
+    'tasks': tasks
+  }
+  return render(request, 'home.html', context)
